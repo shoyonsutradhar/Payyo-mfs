81f56922ba11387642499d4ec3f110cd60d3d151
shoyon sutradhar <shoyonsutradhar070@gmail.com>
2024-09-24 00:02:23 +0600

    add money function

--- js/home.js
+++ js/home.js
@@ -1,1 +1,31 @@
-console.log('home added');
+
+// step-1: add event handlar to the add money button inside the from
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        // relode nai sejonno preventdefault koete hoy 
+        event.preventDefault();
+        // step-2: get money to be added to the account
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        // console.log(addMoneyInput);
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        //  get the pin provided
+        const PinNumberInput = document.getElementById('input-pin-number').value;
+        // console.log(PinNumberInput);
+        // step-3: pin number varify
+        if(PinNumberInput === '12345'){
+            console.log('add money succesfull');
+            // step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            // console.log(balance);
+            const balanceNumber =parseFloat(balance);
+            // calculate 
+            const newBalance = addMoneyNumber + balanceNumber;
+            document.getElementById('account-balance').innerText = newBalance;
+
+            // step-5: add addMoneyInput with balance
+            
+        }
+        else{
+            alert('Failed to add money!Please try again.');
+        }
+    })
